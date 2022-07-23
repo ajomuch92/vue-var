e6cbd92bdc2fb04bce2767bd18e9e3867cd67ee2
ajomuch92 <aaronmontes_92@yahoo.es>
2022-07-23 17:10:23 -0600

    fix: 🐛 Initial commit

---
+++ dev/serve.vue
@@ -0,0 +1,17 @@
+<script>
+import Vue from 'vue';
+import VueVar from '@/vue-var.vue';
+
+export default Vue.extend({
+  name: 'ServeDev',
+  components: {
+    VueVar
+  }
+});
+</script>
+
+<template>
+  <div id="app">
+    <vue-var />
+  </div>
+</template>
