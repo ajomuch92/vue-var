0656a277eb015266c805fa434a6fc3d92fc40536
ajomuch92 <aaronmontes_92@yahoo.es>
2022-07-23 17:28:19 -0600

    feat: 🎸 Adding functionality to component

--- dev/serve.vue
+++ dev/serve.vue
@@ -6,12 +6,29 @@ export default Vue.extend({
   name: 'ServeDev',
   components: {
     VueVar
+  },
+  data: () => ({
+    items: [],
+  }),
+  created() {
+    this.items = Array.from({length: 10}, (_, i) => i + 1);
+  },
+  methods: {
+    myMethod(number) {
+      return Math.pow(number, 2);
+    }
   }
 });
 </script>
 
 <template>
   <div id="app">
-    <vue-var />
+    <ul>
+      <vue-var v-for="item in items" :key="item" :data="myMethod(item)">
+        <li slot="default" slot-scope="{data}">
+          Item {{item}} | My Method {{data}} | {{data % 2 == 0 ? 'Pair':'Odd'}}
+        </li>
+      </vue-var>
+    </ul>
   </div>
 </template>
--- src/vue-var.vue
+++ src/vue-var.vue
@@ -1,78 +1,16 @@
 <script>
-
-export default /*#__PURE__*/{
-  name: 'VueVar', // vue component name
-  data() {
-    return {
-      counter: 5,
-      initCounter: 5,
-      message: {
-        action: null,
-        amount: null,
-      },
-    };
-  },
-  computed: {
-    changedBy() {
-      const { message } = this;
-      if (!message.action) return 'initialized';
-      return `${message.action} ${message.amount || ''}`.trim();
+export default {
+    name: 'VueVar',
+    props: {
+        data: {
+            type: [Object, Array, Number, Boolean, String],
+            required: true,
+        },
     },
-  },
-  methods: {
-    increment(arg) {
-      const amount = (typeof arg !== 'number') ? 1 : arg;
-      this.counter += amount;
-      this.message.action = 'incremented by';
-      this.message.amount = amount;
+    render() {
+        return this.$scopedSlots.default({
+            data: this.data,
+        });
     },
-    decrement(arg) {
-      const amount = (typeof arg !== 'number') ? 1 : arg;
-      this.counter -= amount;
-      this.message.action = 'decremented by';
-      this.message.amount = amount;
-    },
-    reset() {
-      this.counter = this.initCounter;
-      this.message.action = 'reset';
-      this.message.amount = null;
-    },
-  },
 };
 </script>
-
-<template>
-  <div class="vue-var">
-    <p>The counter was {{ changedBy }} to <b>{{ counter }}</b>.</p>
-    <button @click="increment">
-      Click +1
-    </button>
-    <button @click="decrement">
-      Click -1
-    </button>
-    <button @click="increment(5)">
-      Click +5
-    </button>
-    <button @click="decrement(5)">
-      Click -5
-    </button>
-    <button @click="reset">
-      Reset
-    </button>
-  </div>
-</template>
-
-<style scoped>
-  .vue-var {
-    display: block;
-    width: 400px;
-    margin: 25px auto;
-    border: 1px solid #ccc;
-    background: #eaeaea;
-    text-align: center;
-    padding: 25px;
-  }
-  .vue-var p {
-    margin: 0 0 1em;
-  }
-</style>
